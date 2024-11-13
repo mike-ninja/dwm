9859adb70647e5264c266b6fcc4ba87ae908fba1
Michel Philippe Edubas <michel.edubas@steelx.com>
2024-11-14 07:43:54 +1000

    first personal commit

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
+static char *fonts[]          = { "FiraCode Nerd Font:size=10", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -33,12 +33,25 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
-const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-lq", NULL };
+const char *spcmd1[] = { TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd2[] = { TERMINAL, "-n", "spcalc", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-lq", NULL };
+const char *spcmd3[] = { TERMINAL, "-n", "spteams", "-g", "180x54", "-e", "teams-for-linux", NULL };
+const char *spcmd4[] = { TERMINAL, "-n", "spoutlook", "-g", "180x54", "-e", "outlook-for-linux", NULL };
+const char *spcmd5[] = { TERMINAL, "-n", "spfigma", "-g", "180x54", "-e", "figma-linux", NULL };
+const char *spcmd6[] = { TERMINAL, "-n", "spnotes", "-g", "120x40", "-e", "sh", "-c", "cd ~/Notes && nvim", NULL };
+const char *spcmd7[] = { TERMINAL, "-n", "sptelegram", "-g", "50x34", "-e", "telegram-desktop", NULL };
+const char *spcmd8[] = { TERMINAL, "-n", "spdiscord", "-g", "180x54", "-e", "discord", NULL };
+
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
 	{"spcalc",      spcmd2},
+	{"spteams",     spcmd3},
+	{"spoutlook",   spcmd4},
+	{"spfigma",   	spcmd5},
+	{"spnotes",   	spcmd6},
+	{"sptelegram",  spcmd7},
+	{"spdiscord",   spcmd8},
 };
 
 /* tagging */
@@ -53,10 +66,16 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,          1 << 8,      0,           0,          0,         -1 },
 	{ TERMCLASS,  NULL,       NULL,       	 0,           0,           1,          0,         -1 },
 	{ NULL,       NULL,       "Event Tester", 0,          0,           0,          1,         -1 },
-	{ TERMCLASS,  "floatterm", NULL,       	 0,           1,           1,          0,         -1 },
-	{ TERMCLASS,  "bg",        NULL,       	 1 << 7,      0,           1,          0,         -1 },
-	{ TERMCLASS,  "spterm",    NULL,       	 SPTAG(0),    1,           1,          0,         -1 },
-	{ TERMCLASS,  "spcalc",    NULL,       	 SPTAG(1),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "floatterm",  NULL,       	 0,           1,           1,          0,         -1 },
+	{ TERMCLASS,  "bg",         NULL,       	 1 << 7,      0,           1,          0,         -1 },
+	{ TERMCLASS,  "spterm",     NULL,       	 SPTAG(0),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "spcalc",     NULL,       	 SPTAG(1),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "spteams",    NULL,       	 SPTAG(2),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "spoutlook",  NULL,       	 SPTAG(3),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "spfigma",    NULL,          SPTAG(4),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "spnotes",    NULL,          SPTAG(5),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "sptelegram", NULL,          SPTAG(6),    1,           1,          0,         -1 },
+	{ TERMCLASS,  "spdiscord",  NULL,          SPTAG(7),    1,           1,          0,         -1 },
 };
 
 /* layout(s) */
@@ -104,7 +123,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *termcmd[]  = { TERMINAL, NULL };
+static const char *termcmd[]  = { "alacritty", "-e", "tmux", NULL };
 
 /*
  * Xresources preferences to load at startup
@@ -168,7 +187,7 @@ static const Key keys[] = {
 	{ MODKEY,			XK_e,          spawn,                  SHCMD(TERMINAL " -e neomutt ; pkill -RTMIN+12 dwmblocks; rmdir ~/.abook 2>/dev/null") },
 	{ MODKEY|ShiftMask,		XK_e,          spawn,                  SHCMD(TERMINAL " -e abook -C ~/.config/abook/abookrc --datafile ~/.config/abook/addressbook") },
 	{ MODKEY,			XK_r,          spawn,                  {.v = (const char*[]){ TERMINAL, "-e", "lfub", NULL } } },
-	{ MODKEY|ShiftMask,		XK_r,          spawn,                  {.v = (const char*[]){ TERMINAL, "-e", "htop", NULL } } },
+	{ MODKEY|ShiftMask,		XK_r,  togglescratch,          {.ui = 7} },
 	{ MODKEY,			XK_t,          setlayout,              {.v = &layouts[0]} }, /* tile */
 	{ MODKEY|ShiftMask,		XK_t,          setlayout,              {.v = &layouts[1]} }, /* bstack */
 	{ MODKEY,			XK_y,          setlayout,              {.v = &layouts[2]} }, /* spiral */
@@ -199,6 +218,7 @@ static const Key keys[] = {
 	{ MODKEY,			XK_g,          shiftview,              { .i = -1 } },
 	{ MODKEY|ShiftMask,		XK_g,          shifttag,               { .i = -1 } },
 	{ MODKEY,			XK_h,          setmfact,               {.f = -0.05} },
+	{ MODKEY|ShiftMask,		XK_h,          spawn,                  {.v = (const char*[]){ TERMINAL, "-e", "htop", NULL } } },
 	/* J and K are automatically bound above in STACKEYS */
 	{ MODKEY,			XK_l,          setmfact,               {.f = +0.05} },
 	{ MODKEY,			XK_semicolon,  shiftview,              { .i = 1 } },
@@ -210,15 +230,18 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_Return,     togglescratch,          {.ui = 0} },
 
 	{ MODKEY,			XK_z,          incrgaps,               {.i = +3 } },
+	{ MODKEY|ShiftMask,			XK_z, togglescratch,          {.ui = 2} },
 	/* { MODKEY|ShiftMask,		XK_z,          spawn,                  SHCMD("") }, */
 	{ MODKEY,			XK_x,          incrgaps,               {.i = -3 } },
+	{ MODKEY|ShiftMask,			XK_x, togglescratch,          {.ui = 3} },
 	/* { MODKEY|ShiftMask,		XK_x,          spawn,                  SHCMD("") }, */
-	{ MODKEY,			XK_c,          spawn,                  {.v = (const char*[]){ TERMINAL, "-e", "profanity", NULL } } },
-	/* { MODKEY|ShiftMask,		XK_c,          spawn,                  SHCMD("") }, */
+	{ MODKEY,			XK_c,          spawn,                  {.v = (const char*[]){ "chromium", NULL } } },
+	{ MODKEY|ShiftMask,			XK_c, togglescratch,          {.ui = 4} },
 	/* V is automatically bound above in STACKKEYS */
 	{ MODKEY,			XK_b,          togglebar,              {0} },
-	/* { MODKEY|ShiftMask,		XK_b,          spawn,                  SHCMD("") }, */
-	{ MODKEY,			XK_n,          spawn,                  {.v = (const char*[]){ TERMINAL, "-e", "nvim", "-c", "VimwikiIndex", NULL } } },
+	{ MODKEY|ShiftMask,			XK_b, togglescratch,          {.ui = 6} },
+	// { MODKEY|ShiftMask,			XK_b, togglescratch,          {.ui = 4} },
+	{ MODKEY,			XK_n,          togglescratch,          {.ui = 5} },
 	{ MODKEY|ShiftMask,		XK_n,          spawn,                  SHCMD(TERMINAL " -e newsboat ; pkill -RTMIN+6 dwmblocks") },
 	{ MODKEY,			XK_m,          spawn,                  {.v = (const char*[]){ TERMINAL, "-e", "ncmpcpp", NULL } } },
 	{ MODKEY|ShiftMask,		XK_m,          spawn,                  SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)") },
